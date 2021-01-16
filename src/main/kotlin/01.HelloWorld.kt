08108c40195937cc0909f705c42733139ca2b00a
Akansha Jain <akansha.jn111@gmail.com>
2021-01-16 15:48:56 +0530

    Variables

--- src/main/kotlin/01.HelloWorld.kt
+++ src/main/kotlin/01.HelloWorld.kt
@@ -1,3 +1,4 @@
-fun main(){
-    print("Hello World")
-}
+//fun main(){
+//    print("Hello World")
+//
+//}
---
+++ src/main/kotlin/02.Variables.kt
@@ -0,0 +1,10 @@
+fun main() {
+//variables
+    var name = "Akansha" //since it is var value can be changed - Mutable property
+    println(name)
+    val tech = "Android" //val value cannot be reassigned
+    println(tech)
+
+    name="Jain"
+    println(name)
+}
