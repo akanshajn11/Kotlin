dc8a5de6c2f97ca5b148ba1d0b64150308651f5a
Akansha Jain <akansha.jn111@gmail.com>
2021-01-16 16:09:27 +0530

    Types

--- src/main/kotlin/02.Variables.kt
+++ src/main/kotlin/02.Variables.kt
@@ -1,5 +1,5 @@
-fun main() {
-//variables
+fun variables() {
+
     var name = "Akansha" //since it is var value can be changed - Mutable property
     println(name)
     val tech = "Android" //val value cannot be reassigned
---
+++ src/main/kotlin/03.Types.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    //number
+    var byte : Byte =0 //8 bit
+    var short:Short=0 // 16 bit
+    var int:Int=0 //32 bit
+    var long: Long =0L //64 bit
+    var float:Float=0F // 32 bit
+    var double:Double=0.0 //64 bit
+
+    println(byte)
+    println(short)
+    println(int)
+    println(long)
+    println(float)
+    println(double)
+
+    //strings
+    var string: String="Akansha"
+    var char:Char='J'
+
+    //booleans
+    var boolean:Boolean=true
+
+}
