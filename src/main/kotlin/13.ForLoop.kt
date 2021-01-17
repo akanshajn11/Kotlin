3b7698b339663c311839238b318aa87c2cafec5f
Akansha Jain <akanshajn01@gmail.com>
2021-01-17 11:58:26 +0530

    For loops

---
+++ src/main/kotlin/13.ForLoop.kt
@@ -0,0 +1,42 @@
+fun main()
+{
+    val list = listOf("Akansha","Jain","AJ")
+
+    for (name in list){
+        println(name)
+    }
+
+    for (number in 1..100)
+    {
+        println(number)
+    }
+
+    //to exclude the last boundary
+    for(number in 0 until 100){
+        println(number)
+    }
+
+    //to increment by some number
+    for(number in 0 until 100 step 5){
+        println(number)
+    }
+
+    //run loop in descending order
+    for(number in 100 downTo 0 step 10)
+    {
+        println(number)
+    }
+
+    //to loop on indices
+    for(index in list.indices)
+    {
+        println(index)
+        println(list[index])
+    }
+
+    //considering both index and value in loop
+    for((index,value) in list.withIndex()){
+        println(index)
+        println(value)
+    }
+}
